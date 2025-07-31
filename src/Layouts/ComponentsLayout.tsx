67512a51f66c7874d043689b26c43b7eea13a908
michee2x <michaelisraelmike@gmail.com>
2025-07-31 20:14:09 +0100

    commit

--- src/Layouts/ComponentsLayout.tsx
+++ src/Layouts/ComponentsLayout.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { ComponentJson } from '../Components/Componentsjson';
 import { Link, Outlet } from 'react-router-dom';
 import { RxComponent1 } from "react-icons/rx";
+import { LiaTimesSolid } from "react-icons/lia";
 
 const ComponentsLayout = () => {
   const [showSidebar, setShowSidebar] = React.useState(false);
   return (
-    <div className="w-screen relative min-h-screen lg:pt-0 pt-5 text-white">
+    <div className="w-screen relative min-h-screen lg:pt-0 text-white">
       <main className="w-full flex gap-6 h-auto lg:p-4">
         {/* Sidebar */}
         <aside
@@ -16,12 +17,13 @@ const ComponentsLayout = () => {
               "--radix-scroll-area-corner-height": "0px",
             } as React.CSSProperties
           }
-          className={`lg:w-[250px] w-full  sticky top-16 shrink-0 h-auto lg:h-[calc(100vh-3.5rem)] overflow-y-auto p-4 border-r border-slate-800 scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-transparent hover:scrollbar-thumb-zinc-500 transition-colors duration-300 ${
+          className={`lg:w-[250px] bg-black z-[90000] w-full  sticky top-16 shrink-0 h-auto lg:h-[calc(100vh-3.5rem)] overflow-y-auto p-4 border-r border-slate-800 scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-transparent hover:scrollbar-thumb-zinc-500 transition-colors duration-300 ${
             showSidebar ? "block" : "hidden lg:block"
           }`}
         >
-          <div>
-            <h2 className="text-xl font-semibold mb-4">Categories</h2>
+          <div className='flex justify-between mt-10 mb-4 items-center px-2'>
+            <h2 className="text-xl font-semibold">Categories</h2>
+            <LiaTimesSolid className='text-2xl' onClick={() => setShowSidebar(false)} />
           </div>
           <ul className="space-y-2">
             {Object.keys(ComponentJson).map((category, index) => (
@@ -44,15 +46,21 @@ const ComponentsLayout = () => {
         <div
           className={`lg:w-[80%] ${
             !showSidebar ? "block" : "hidden lg:block"
-          } w-full mt-20 lg:mt-10 flex-col gap-6 flex justify-center`}
+          } w-full flex-col relative flex justify-center`}
         >
           <div
-            onClick={() => setShowSidebar(prev => !prev)}
-            className="text-3xl pl-4 lg:hidden text-white"
+            onClick={() => setShowSidebar((prev) => !prev)}
+            className="text-3xl flex h-[9rem] bg-black py-6 z-[10000] sticky top-0 w-full items-end justify-between px-4 lg:hidden text-white"
           >
             <RxComponent1 />
+
+            <span className="text-[12px] text-gray-500">
+              Components Category
+            </span>
           </div>
-          <Outlet />
+          <div className="w-full h-auto lg:mt-10">
+            <Outlet />
+          </div>
         </div>
       </main>
     </div>
--- src/Layouts/PagesLayout.tsx
+++ src/Layouts/PagesLayout.tsx
@@ -6,22 +6,21 @@ const PagesLayout = () => {
   return (
     <div className="relative max-w-[100vw] flex justify-center w-screen min-h-[120vh] text-white bg-white font-batman font-bold">
       <Nav />
-      <div className="w-full relative h-full bg-black"
-      >
+      <div className="w-full relative h-full bg-black">
         <Outlet />
-      </div>
-      <div className="w-auto h-auto text-2xl gap-3 p-4 flex flex-col items-end z-10 text-black absolute bottom-0 right-0">
-        <div className="w-full h-auto justify-end min-w-fit flex items-center gap-4">
-          <FaDiscord />
-          <FaGithub />
-          <FaTwitter />
+        <div className="w-auto h-auto text-2xl gap-3 p-4 flex flex-col items-end z-10 text-white">
+          <div className="w-full h-auto justify-end min-w-fit flex items-center gap-4">
+            <FaDiscord />
+            <FaGithub />
+            <FaTwitter />
+          </div>
+          <p className="text-[16px]">
+            built by{" "}
+            <a className="underline" href="https://github.com/michee2x">
+              michee2x
+            </a>
+          </p>
         </div>
-        <p className="text-[16px]">
-          built by{" "}
-          <a className="underline" href="https://github.com/michee2x">
-            michee2x
-          </a>
-        </p>
       </div>
     </div>
   );
